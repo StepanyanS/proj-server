01f8aab2a62f5c8e926463e3e28b5841927b1f1d
StepanyanS <infostepanyans@gmail.com>
2018-08-28 21:03:39 +0400

    app comments

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+// import modules
 import * as express from 'express';
 import * as cors from 'cors';
 import { json } from 'body-parser';
@@ -8,16 +9,31 @@ import { Routes } from './routes/index';
 // import moddlewares
 import { passportMiddleware } from './middleware/passport';
 
+/**
+ * @description Express application instance
+ * @export
+ * @class App
+ */
 export class App {
   routes: Routes;
   expressApp: express.Express;
   corsOptions: cors.CorsOptions;
 
+  
+  /**
+   * @description Creates an instance of App
+   * @memberof App
+   */
   constructor() {
     this.routes = new Routes();
   }
 
-  bootstrap() {
+  
+  /**
+   * @description Boostrapping Express application
+   * @memberof App
+   */
+  bootstrap(): void {
 
     this.expressApp = express();
 
@@ -31,7 +47,7 @@ export class App {
 
     this.expressApp.use('/api', this.routes.router);
 
-    this.expressApp.listen(3000, () => {
+    this.expressApp.listen(3000, (): void => {
       console.log('Server started!');
     });
   }
