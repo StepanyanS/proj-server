f9cba3150b555b1ed241ab90172b7878317f222c
StepanyanS <infostepanyans@gmail.com>
2018-08-28 20:33:09 +0400

    code beautify

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,12 +8,27 @@ import { ProjectsRouter } from './projects-router';
 const usersrouter = new UsersRouter();
 const projectsRouter = new ProjectsRouter();
 
+/**
+ * @description Routes instance
+ * @export
+ * @class Routes
+ * @extends {Routing}
+ */
 export class Routes extends Routing {
 
+  /**
+   * @description Creates an instance of Routes
+   * @memberof Routes
+   */
   constructor() {
     super();
   }
 
+  
+  /**
+   * @description Adds main routes
+   * @memberof Routes
+   */
   route(): void {
 
     this.router.use('/users/', usersrouter.router);
